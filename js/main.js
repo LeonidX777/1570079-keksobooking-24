a392d8c5bc1a2d9ad36b7b59aae2bac138e571fc
Леонид Харитонов <61547271+LeonidX777@users.noreply.github.com>
2021-09-30 00:28:32 +0300

    Нужно больше функций. Правки

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
-const randomInteger=(min, max) => {
-  // получить случайное число от (min-0.5) до (max+0.5)
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
+const getRandomIntegerInRange = (min, max) => {
+  const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
-}
+};
 
- randomInteger(1, 3);
+getRandomIntegerInRange(1, 3);
 
+const getRandomIntToMax = (max) =>
+  Math.floor(Math.random() * max);
 
-
- const getRandomInt=(max) => {
-  return Math.floor(Math.random() * max);
-}
-
- getRandomInt(3);
-// expected output: 0, 1 or 2
-
-getRandomInt(1);
-// expected output: 0
-
-Math.random();
-// expected output: a number from 0 to <1
+getRandomIntToMax(1);
