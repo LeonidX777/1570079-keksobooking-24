e1728ea09047caf0b5e594cbee15eb8d1e94070d
Леонид Харитонов <61547271+LeonidX777@users.noreply.github.com>
2021-10-26 22:04:19 +0300

    Отрисуй меня полностью (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {createCard} from './templates/offer.js';
 import {getAnnouncement} from './mock-data.js';
+import {setPageEnabled, setPageDisabled} from './page-activity.js';
 
 const MAX_COUNT = 10;
 
@@ -10,3 +11,6 @@ const container = document.querySelector('#map-canvas');
 announcements.forEach((offer) => {
   container.appendChild(createCard(offer));
 });
+
+setPageDisabled();
+setPageEnabled();
---
+++ js/page-activity.js
@@ -0,0 +1,37 @@
+const setPageDisabled = () => {
+  const formAd = document.querySelector('.ad-form');
+  formAd.classList.add('ad-form--disabled');
+
+  const fieldsets = formAd.querySelectorAll('fieldset');
+  for (const element of fieldsets) {
+    element.disabled = true;
+  }
+
+  const formFilters = document.querySelector('.map__filters');
+  formFilters.classList.add('map__filters--disabled');
+
+  const filterElements = formFilters.children;
+  for (const element of filterElements) {
+    element.disabled = true;
+  }
+};
+
+const setPageEnabled = () => {
+  const formAd = document.querySelector('.ad-form');
+  formAd.classList.remove('ad-form--disabled');
+
+  const fieldsets = formAd.querySelectorAll('fieldset');
+  for (const element of fieldsets) {
+    element.disabled = false;
+  }
+
+  const formFilters = document.querySelector('.map__filters');
+  formFilters.classList.remove('map__filters--disabled');
+
+  const filterElements = formFilters.children;
+  for (const element of filterElements) {
+    element.disabled = false;
+  }
+};
+
+export {setPageDisabled, setPageEnabled};
